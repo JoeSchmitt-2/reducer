9854d91fc6236806670614d5c3bd1cbfad563cae
JoeSchmitt-2 <84737443+JoeSchmitt-2@users.noreply.github.com>
2023-08-03 20:11:03 -0500

    updated return app.js

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function TaskApp() {
 
 	return (
 		<>
-			<h1>Prague itinerary</h1>
+			<h1>Grocery List for Aldi's</h1>
 			<AddTask onAddTask={handleAddTask} />
 			<TaskList
 				tasks={tasks}
@@ -43,7 +43,7 @@ export default function TaskApp() {
 
 let nextId = 3;
 const initialTasks = [
-	{ id: 0, text: "Visit Kafka Museum", done: true },
-	{ id: 1, text: "Watch a puppet show", done: false },
-	{ id: 2, text: "Lennon Wall pic", done: false },
+	{ id: 0, text: "bluberries", done: true },
+	{ id: 1, text: "strawberries", done: false },
+	{ id: 2, text: "kiwi", done: false },
 ];
